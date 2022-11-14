94eed9b1569aace82d7d58660a54af2c755a9d2b
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-11-14 21:57:08 +0100

    TS Commit

--- 4_Type_Aliasing/dist/index.js
+++ 4_Type_Aliasing/dist/index.js
@@ -9,16 +9,45 @@ const obj = [
     },
 ];
 const myColor = "blue";
-const colorArray = ["blue", "green", "red", "magenta", "cyan"];
+const colorArray = [
+    "blue",
+    "green",
+    "red",
+    "magenta",
+    "cyan",
+];
 const tümColors = ["cyan", "green", "magenta", "red"];
 const persondog = {
-    "yas": 12,
-    "tur": "j",
-    "cins": "asd",
+    yas: 12,
+    tur: "j",
+    cins: "asd",
 };
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+const benimMap = new Map(); //! Burada Map kullanarak ilk parametreye string, ikinciye Kisiyi verdik. Böylece kisinin icerisine erismis olduk.
+benimMap.set("kadir", {
+    isim: "Kadir",
+    soyisim: "Inanir",
+    yas: 70
+});
 const myMap = new Map();
 myMap.set("Ferhat", {
-    isim: "Ferhat",
+    isim: "Faso",
     soyisim: "asd",
     yas: 12,
 });
+console.log(myMap);
+const silObj = [
+    {
+        vorname: "Ferhat",
+        nachname: "Dursun",
+        alt: 30
+    }
+];
+const silMySehir = "Dortmund";
+const sonSehir = ["Dortmund", "Münster", "Paderborn", "Istanbul", "Eskisehir", "Bartin"];
+const Insan = {
+    isim: "Serafettin",
+    soyisim: "Dursun",
+    memleket: "Bartin",
+    yas: 28
+};
