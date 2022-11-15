4e2e4f40b80647811271540891b94127eea3f6e9
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-11-15 09:14:57 +0100

    TS Commit

--- 6_Object_Typing/src/index.ts
+++ 6_Object_Typing/src/index.ts
@@ -18,7 +18,7 @@ type MyObject = {
 
   yas: string;
 
-  [key: string]: string | number; //! Bu sekilde (any Type'i olarak) yazildiginda key alt da(myobject) zorunlu olarak kullanilmak durumunda olmuyor. Ama bunun icin Type destegi alamiyoruz.
+  [key: string]: string | number; //! Bu sekilde (any Type'i olarak) yazildiginda key alt da(myobject) zorunlu olarak kullanilmak durumunda olmuyor. VE bu key herhangi birsey olabilir!!! Ama bunun icin Type destegi alamiyoruz.
 };
 
 const myobject: MyObject = {
@@ -29,8 +29,42 @@ const myobject: MyObject = {
 
 //?????????????????????????????????
 
-const Objelerim: Record<string, boolean> = {
-  acikMi: true,
+const Objelerim: Record<string, {yas: number}> = {
+  acikMi: {yas:30}
 };
 
 //! Bu sekilde de kullanimi mevcuttur.
+
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+type silPerson = {
+  vorname: string,
+  nachname: string,
+  alt?: number,
+  [key: string]: any
+}
+
+const silperson: silPerson = {
+  vorname: "Ferhat",
+  nachname: "Dursun",
+  yas: 30
+}
+
+
+const silMyCiro: Record<number, { ciro: number }> = {
+  2020: {
+    ciro: 4.3
+  }
+}
+
+const myGeld: Record<number, { mygeld: number }> = {
+  2022: {
+    mygeld: 2.1
+  }
+}
