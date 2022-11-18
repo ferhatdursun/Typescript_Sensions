c6cfb2ddb33a8a9e49a4ca879a6bae9d29c83868
Ferhat Dursun <ferhatdursnu@gmail.com>
2022-11-18 16:25:43 +0100

    TS_Sessions

---
+++ Udemy_Türkce/app.js
@@ -0,0 +1,12 @@
+console.log("Typescriptsssss");
+//! Eger bir degiskenin degerini Program icerisinde degistireceksem let kullaniyorum,
+//! degistirmeyeceksem const ile tanimliyorum.
+var age = 30;
+var name2 = "Ferhat";
+var soyisim = "Dursun";
+console.log(name2);
+var isimSoyisim = "Benim adim ".concat(name2, ", soyadim ").concat(soyisim);
+console.log(isimSoyisim);
+function display(id, name) {
+    console.log("id = " + id + ", Name =" + name);
+}
---
+++ Udemy_Türkce/app.ts
@@ -0,0 +1,16 @@
+console.log("Typescriptsssss");
+//! Eger bir degiskenin degerini Program icerisinde degistireceksem let kullaniyorum,
+//! degistirmeyeceksem const ile tanimliyorum.
+let age: number = 30;
+let name2: string = "Ferhat"
+let soyisim: string = "Dursun"
+console.log(name2);
+
+let isimSoyisim: string = `Benim adim ${name2}, soyadim ${soyisim}`
+console.log(isimSoyisim);
+
+
+function display(id: any, name: string) {
+    console.log("id = " + id + ", Name =" + name);   
+}
+
